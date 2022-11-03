2b5c6945e72884275f9ece501e32371c106ed4ba
codingfun2022 <codingfun2022@163.com>
2022-11-03 11:14:08 +0000

    Install PyTorch, PySpark and MetaSpore.

--- .env.restore.sh
+++ .env.restore.sh
@@ -10,5 +10,8 @@ rm -rf $(realpath $(dirname ${BASH_SOURCE[0]}))/.env
 python -m venv $(realpath $(dirname ${BASH_SOURCE[0]}))/.env
 source $(realpath $(dirname ${BASH_SOURCE[0]}))/.env/bin/activate
 python -m pip install --upgrade pip
+python -m pip install torch==1.11.0+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+python -m pip install https://ks3-cn-beijing.ksyuncs.com/dmetasoul-bucket/releases/spark/pyspark-3.1.2.265f9ad4ee-py2.py3-none-any.whl
+python -m pip install metaspore~=1.1.0
 python -m pip freeze > $(realpath $(dirname ${BASH_SOURCE[0]}))/.env/requirements.txt
 cp -f $(realpath $(dirname ${BASH_SOURCE[0]}))/.env/requirements.txt $(realpath $(dirname ${BASH_SOURCE[0]}))
